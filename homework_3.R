2f0c4c5c94a137ef9f51195283957a5b1e67878c
QdziaK <99800648+QdziaK@users.noreply.github.com>
2022-02-25 22:10:01 +0000

    Data clean up
    Loaded in the stroop data, summarised and made the initial clean up

--- homework_3.R
+++ homework_3.R
@@ -1,1 +1,29 @@
 # Homework 3
+
+library(tidyverse)
+library(lme4)
+stroop_d <- read.csv("stroop_standing_data.csv")
+
+# A within subjects 3 (data type: practice vs congruent vs incongruent) x2 (position: standing vs sitting) Anova can be conducted to analyze whether there is an effect
+# of the manipulation of sitting vs standing.
+
+# let's examine the data
+summary(stroop_d)
+
+# We need to clean up the data so they are good for the analysis
+# it feels like something is missing here in the information that we were given because I do not know what the X is for
+# I will drop X as it does not denote anything of interest really
+d = subset(stroop_d, select = -c(X) )
+# Now that is done, we might want to drop the trials that had NA
+
+# there is also a practice condition which might need to be included?
+# anyway, analysis
+# let's first create the means for all the participants
+means <- aggregate(stroop_d$rt,
+                         by = list(stroop_d$subject, stroop_d$phase,
+                                   stroop_d$congruency),
+                         FUN = 'mean')
+
+multi_level_model_intercepts <- lmer(y ~ 1 + x + (x|subject), stroop_d)
+
+

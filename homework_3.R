2c4c7363958b54c65345e7e7708a22f5729f8a0e
QdziaK <99800648+QdziaK@users.noreply.github.com>
2022-02-25 23:35:29 +0000

    adding spotify
    Added the spotify data, attempted the first part of analysis

--- homework_3.R
+++ homework_3.R
@@ -13,10 +13,16 @@ summary(stroop_d)
 # We need to clean up the data so they are good for the analysis
 # it feels like something is missing here in the information that we were given because I do not know what the X is for
 # I will drop X as it does not denote anything of interest really
-d = subset(stroop_d, select = -c(X) )
+d = subset(stroop_d, select = -c(X))
 # Now that is done, we might want to drop the trials that had NA
+nrow(d)
+no_missing_vals <- drop_na(d)
+df = subset(stroop_d, drop = rt == "FALSE")
+?drop
+dropped <- drop(stroop_d$rt == "FALSE")
 
 # there is also a practice condition which might need to be included?
+# what is the baseline congruency?
 # anyway, analysis
 # let's first create the means for all the participants
 means <- aggregate(stroop_d$rt,
@@ -26,4 +32,18 @@ means <- aggregate(stroop_d$rt,
 
 multi_level_model_intercepts <- lmer(y ~ 1 + x + (x|subject), stroop_d)
 
+# 3 
+# load the data
+library(readr)
+spotify_cleaned <- read_csv("spotify_cleaned.csv")
 
+# examine the data
+summary(spotify_cleaned)
+
+spotify_for_prc <- select(spotify_cleaned, -"Track Name", -"Artist",-"Streams", -"key", -"mode", -"time_signature", -"duration_sec")
+prc_spotify <- prcomp(spotify_for_prc)
+summary(prc_spotify)
+
+spotify_for_prc_2 <- select(spotify_cleaned, -"Track Name", -"Artist",-"Streams", -"time_signature",-"duration_sec")
+prc_spotify_2 <- prcomp(spotify_for_prc_2)
+summary(prc_spotify_2)
